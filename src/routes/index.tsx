533b36787c1842fbc055186abf7e6bd77c549514
Eriec Tanijaya <eriectan88@gmail.com>
2025-12-13 21:44:18 +0700

    feat(hero): basic init

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,7 +10,13 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { ListIcon, TagIcon, TranslateIcon, XIcon } from "@phosphor-icons/react";
+import {
+  ListIcon,
+  SparkleIcon,
+  TagIcon,
+  TranslateIcon,
+  XIcon,
+} from "@phosphor-icons/react";
 import { Badge } from "@/components/ui/badge";
 import { useIsMobile } from "@/hooks/use-mobile";
 import {
@@ -107,9 +113,37 @@ function App() {
         </div>
       </header>
 
-      <main className="max-w-7xl mx-auto">
+      <main className="max-w-7xl">
+        <section className="flex flex-col bg-primary px-4 gap-3 items-center py-20">
+          <div className="flex rounded-full text-sm px-4 py-2 bg-primary-foreground/10 text-primary-foreground/90 mb-9">
+            <SparkleIcon className="size-4" />
+            Community-powered book sharing
+          </div>
+          <h1 className="text-4xl font-bold tracking-tight text-primary-foreground">
+            Read, Share, and Connect
+          </h1>
+          <p className="text-primary-foreground/80">
+            Imagine borrowing books shared by fellow members.
+          </p>
+
+          <div className="flex gap-3 mt-9">
+            <Button
+              size="lg"
+              className="bg-primary-foreground text-primary font-semibold shadow-lg"
+            >
+              Browse Books
+            </Button>
+            <Button
+              size="lg"
+              className="border-2 border-primary-foreground/30 bg-transparent text-primary-foreground"
+            >
+              How It Works
+            </Button>
+          </div>
+        </section>
+
         <h2 className="text-xl font-bold ml-4 my-4">Featured Books</h2>
-        <div className="flex justify-between overflow-auto gap-3">
+        <section className="flex justify-between overflow-auto gap-3">
           <Card className="flex flex-col bg-white pt-0 w-full md:w-[280px] first:ml-4 last:mr-4">
             <CardHeader className="p-0 relative">
               <img
@@ -273,7 +307,7 @@ function App() {
               </Button>
             </CardFooter>
           </Card>
-        </div>
+        </section>
       </main>
     </>
   );
